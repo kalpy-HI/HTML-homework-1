e0ce797633b2cfcdc6e3669ba1eff8ed48669b6b
kalpy-HI <37013995+kalpy-HI@users.noreply.github.com>
2018-03-13 16:40:30 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,11 +11,39 @@ public class Main{
 			if(a==0&&b==0)break;
 			
 			String something = sc.nextLine();
-			String[] array = new String[4];
+			String[] array = new String[a];
+			int[] answer = new int[a][b];
 			
 			for(int i=0;i<3;i++){
 				array = sc.nextLine();
 			}
 			
+			for(int i =0;i<a;i++){
+				for(int j =0;j<b;j++){
+					if('*'==array.charAt[i][j]){
+						answer[i][j]= -1;
+					}
+					else if('.'==array.charAt[i][j]){
+						answer[i][j]= 0;
+					}
+				}
+			}
 			
+			for(int i =0;i<a;i++){
+				for(int j =0;j<b;j++){
+					if(-1==answer.[i][j]){
+						for(int k=i-1;k<=i+1;k++){
+							for(int l=j-1;l<=j+1;l++){
+								if(k>=0&&l>=0){	
+								   if(answer[k][l]!=-1){
+								      answer[k][l]++;
+								   }
+								}
+							}
+						}
+					}
+				}
+			}
 		}
+	}
+}
