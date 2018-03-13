89788538f4a8b3f93a9edfe3aca2d9c7d82c795a
kalpy-HI <37013995+kalpy-HI@users.noreply.github.com>
2018-03-13 11:59:24 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String[] argc){
+		Scanner sc = new Scanner(System.in);
+		
+		for(;;){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a==0&&b==0)break;
+			
+			String something = sc.nextLine();
+			String[] array = new String[4];
+			
+			for(int i=0;i<3;i++){
+				array = sc.nextLine();
+			}
+			
+			
+		}
